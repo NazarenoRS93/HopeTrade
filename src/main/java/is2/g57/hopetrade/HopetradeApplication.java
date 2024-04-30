cd95d954765b23a76af524265310f1b290b45453
Nazareno Rodriguez <rodsimonnaz@gmail.com>
2024-04-30 20:16:53 -0300

    INITIAL COMMIT

---
+++ src/main/java/is2/g57/hopetrade/HopetradeApplication.java
@@ -0,0 +1,13 @@
+package is2.g57.hopetrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopetradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopetradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/is2/g57/hopetrade/HopetradeApplicationTests.java
@@ -0,0 +1,13 @@
+package is2.g57.hopetrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopetradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
