f92db1e4bd02d937b2859265c94ab3cf34012354
unknown <facu.rojasberna@gmail.com>
2024-05-16 17:18:06 -0300

    Problema en mi rama local con la de lauty

--- src/main/java/is2/g57/hopetrade/controller/FilialController.java
+++ src/main/java/is2/g57/hopetrade/controller/FilialController.java
@@ -14,16 +14,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import is2.g57.hopetrade.entity.Filial;
-<<<<<<< HEAD
-import is2.g57.hopetrade.repository.FilialRepository;
-=======
 import is2.g57.hopetrade.entity.Ayudante;
 import is2.g57.hopetrade.repository.AyudanteRepository;
 import is2.g57.hopetrade.repository.FilialRepository;
 
 
 
->>>>>>> branch-lauty3
 
 @RestController
 @RequestMapping("/filial")
@@ -31,37 +27,23 @@ public class FilialController {
 
 	@Autowired
 	private FilialRepository filialRepository;
-<<<<<<< HEAD
-
-	@GetMapping("/{id}")
-	public ResponseEntity<Filial> ObtenerFilialPorId(@PathVariable Long id) {
-=======
 	private AyudanteRepository ayudanteRepository;
 
 	@GetMapping("/id")
 	public ResponseEntity<?> ObtenerFilialPorIdd (@RequestBody FilialRequest filialRequest){
 		Long id = filialRequest.getId();
->>>>>>> branch-lauty3
 		Optional<Filial> filialOp = this.filialRepository.findById(id);
 		if (filialOp.isPresent()) {
 			return new ResponseEntity<>(filialOp.get(), HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>("No se encontro la filial",HttpStatus.NOT_FOUND);
 		}
-<<<<<<< HEAD
-
-	}
-
-	@GetMapping("/{direccion}")
-	public ResponseEntity<Filial> ObtenerFilialPorDireccion(@PathVariable String direccion) {
-=======
 	
 }
 	
 	@GetMapping("/direccion")
 	public ResponseEntity<?> ObtenerFilialPorDireccion(@RequestBody FilialRequest filialRequest) {
 		String direccion = filialRequest.getDireccion();
->>>>>>> branch-lauty3
 		Optional<Filial> filialOp = this.filialRepository.findByDireccion(direccion);
 		if (filialOp.isPresent()) {
 			return new ResponseEntity<>(filialOp.get(), HttpStatus.OK);
@@ -69,13 +51,6 @@ public class FilialController {
 			return new ResponseEntity<>("No se encontro la filial",HttpStatus.NOT_FOUND);
 		}
 	}
-<<<<<<< HEAD
-
-	@GetMapping("/{filial}")
-	public ResponseEntity<?> GuardarFilial(String nombre, String direccion, String email) {
-		try {
-			Filial filial = new Filial(nombre, direccion, email);
-=======
 	
 	@PostMapping("/save")
 	public ResponseEntity<?> GuardarFilial(@RequestBody FilialRequest filialRequest) {
@@ -84,17 +59,12 @@ public class FilialController {
 		}
 		try {
 						Filial filial = new Filial(filialRequest.getNombre(),filialRequest.getDireccion(),filialRequest.getEmail());
->>>>>>> branch-lauty3
 			this.filialRepository.save(filial);
 			return new ResponseEntity<>("Filial registrada", HttpStatus.CREATED);
 		} catch (Exception e) {
 			return new ResponseEntity<>("La direccion ya pertenece a otra filial", HttpStatus.BAD_REQUEST);
 		}
 	}
-<<<<<<< HEAD
-
-}
-=======
 	
 	
 		@PostMapping("/ingresar-ayudante")  ResponseEntity<?> IngresarAyudante (@PathVariable(value = "id_ayudante") Long IdAyudante, @PathVariable (value = "id_filial") Long IdFilial){
@@ -125,4 +95,3 @@ public class FilialController {
 		
 	
 }			
->>>>>>> branch-lauty3
