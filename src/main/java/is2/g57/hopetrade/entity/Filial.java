5f5f6812e98b63920eaf02d172495a498ad027b1
Lauty1550 <lautyjaime09@gmail.com>
2024-05-18 16:43:57 -0300

    Corregi el metodo ingresar ayudante a filial y listar ayudantes de la filial

--- src/main/java/is2/g57/hopetrade/entity/Filial.java
+++ src/main/java/is2/g57/hopetrade/entity/Filial.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -23,6 +25,7 @@ public class Filial {
 	private String email;
 	
 	 @OneToMany(mappedBy = "filial")
+	 @JsonManagedReference
 	  private List<Ayudante> ayudantes;
 
 	public Filial(Long id, String nombre, String direccion, String email) {
