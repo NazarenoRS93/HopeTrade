8e828a1db84343fb6cacaeb01bbcb24fb1e2efa0
matilozano96 <74582367+matilozano96@users.noreply.github.com>
2024-05-24 15:22:11 -0300

    Implementacion de Categorias
    - Entidad
    - Repo
    - Runner para pre-cargar categorias al inicializar BD

---
+++ src/main/java/is2/g57/hopetrade/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package is2.g57.hopetrade.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import is2.g57.hopetrade.entity.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
