20b9c7c4896f36d5a478adf467d9b071f7c0ab2d
Nazareno Rodriguez <rodsimonnaz@gmail.com>
2024-06-10 11:30:27 -0300

    Label para filtro de categoría

--- src/main/frontend/src/containers/PostListPage.js
+++ src/main/frontend/src/containers/PostListPage.js
@@ -7,6 +7,9 @@ import {defaultFormAddPost} from "../utils/utilConstants";
 import Select from '@mui/material/Select';
 import Typography from '@mui/material/Typography';
 import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import {FormLabel, Stack} from "@mui/material";
 
 function PostListPage() {
     // Render on start
@@ -23,8 +26,9 @@ function PostListPage() {
     const reader = new FileReader();
     const [user, setUser] = useState({});
     const [publicaciones, setPublicaciones] = useState([]);
+    const [hayPublis, setHayPublis] = useState(true);
     const [categorias, setCategorias] = useState([]);
-    const [selectedCategoria, setSelectedCategoria] = useState([]);
+    const [selectedCategoria, setSelectedCategoria] = useState(0);
     const [states, setStates] = useState([]);
     const [form, setForm] = useState(defaultFormAddPost)
 
@@ -49,6 +53,8 @@ function PostListPage() {
                     return publicacion.categoria_ID == cat;
                 });
             }
+            if (data.length > 0) setHayPublis(true);
+            else setHayPublis(false);
             setPublicaciones(data);
         } catch (error) {
             console.log("Error obteniendo publicaciones: " + error);
@@ -74,28 +80,40 @@ function PostListPage() {
     }
 
     return (
-        
+
         <React.Fragment>
-			<Select
-				value={selectedCategoria}
-				onChange={handleChange}
-				defaultValue="0"
-				sx={{ minWidth: 200, marginBottom: 2 }}
-			>   
-				<MenuItem value="0">
-					Todos
-				</MenuItem>
-				{categorias.map((categoria) => (
-					<MenuItem key={categoria.id} value={categoria.id}>
-						{categoria.nombre}
-					</MenuItem>
-				))}
-			</Select>
+            <Stack spacing={2} direction="row" justifyContent="center" sx={{paddingBottom: "30px"}}>
+                <FormControl>
+                    <FormLabel>Categoría</FormLabel>
+                    <Select
+                        value={selectedCategoria}
+                        onChange={handleChange}
+                        defaultValue="0"
+                        sx={{ minWidth: 250 }}
+                    >
+                        <MenuItem value="0">
+                            Todos
+                        </MenuItem>
+                        {categorias.map((categoria) => (
+                            <MenuItem key={categoria.id} value={categoria.id}>
+                                {categoria.nombre}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                    <FormHelperText id="categoria-text">Filtre por categoría</FormHelperText>
+                </FormControl>
+            </Stack>
             <PostGrid>
-            { publicaciones.map((publicacion) => (
-                <PostItem id={publicacion.id} data={publicacion} user={user} update={onUpdate}/>
-            ))}
+                { publicaciones.map((publicacion) => (
+                    <PostItem id={publicacion.id} data={publicacion} user={user} update={onUpdate}/>
+                ))}
             </PostGrid>
+            { !hayPublis ?
+                <div style={{textAlign: "center", paddingTop: "30px", paddingBottom: "50px"}}>
+                    <Typography variant="h1">No se encontraron publicaciones.</Typography>
+                </div>
+                : null
+            }
         </React.Fragment>
     )
 }
