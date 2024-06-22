30227b12c06c1ba98fc5eb07c218c138b85ad5ee
Facurojas0176 <facu.rojasberna@gmail.com>
2024-06-22 16:43:36 -0300

    modificacion a respuestaComentario controller

--- src/main/java/is2/g57/hopetrade/controller/RespuestaComentarioController.java
+++ src/main/java/is2/g57/hopetrade/controller/RespuestaComentarioController.java
@@ -1,5 +1,6 @@
 package is2.g57.hopetrade.controller;
 
+import is2.g57.hopetrade.entity.Comentario;
 import is2.g57.hopetrade.entity.RespuestaComentario;
 import is2.g57.hopetrade.repository.RespuestaComentarioRepository;
 import is2.g57.hopetrade.services.MailService;
@@ -16,41 +17,54 @@ import java.util.Optional;
 @RequestMapping("/respuesta-comentario")
 public class RespuestaComentarioController {
 
-    @Autowired
-    private RespuestaComentarioRepository respuestaComentarioRepository;
+	@Autowired
+	private RespuestaComentarioRepository respuestaComentarioRepository;
 
-    @Autowired
-    private MailService mailService; // Asumiendo que tienes un servicio de correo electrónico
-    
-    
-    @GetMapping("/{id}")
-	public ResponseEntity<?> ObtenerRespuestaAComentario(@RequestBody RespuestaComentarioRequest respComentarioRequest) {
-		Optional<RespuestaComentario> respComentarioOp = this.respuestaComentarioRepository.findById(respComentarioRequest.getId());
+	@Autowired
+	private MailService mailService; // Asumiendo que tienes un servicio de correo electrónico
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> ObtenerRespuestaAComentario(
+			@RequestBody RespuestaComentarioRequest respComentarioRequest) {
+		Optional<RespuestaComentario> respComentarioOp = this.respuestaComentarioRepository
+				.findById(respComentarioRequest.getId());
 		if (respComentarioOp.isPresent()) {
 			return new ResponseEntity<>(respComentarioOp.get(), HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>("No se encontro Ayudante", HttpStatus.NOT_FOUND);
 		}
-	} 
+	}
 
-
-    @PostMapping("/responder")
-    public ResponseEntity<?> guardarRespuestaDeComentario(@RequestBody RespuestaComentarioRequest respComentarioRequest) {
-        try {
-        	// Crear una nueva respuesta de comentario
-            RespuestaComentario nuevaRespuesta = new RespuestaComentario(
-                    respComentarioRequest.getText(),
-                    respComentarioRequest.getUser(),
-                    respComentarioRequest.getComentario()
-            );
-            // Guardar la nueva respuesta en la base de datos
-           respuestaComentarioRepository.save(nuevaRespuesta);
-            // Puedes enviar un correo electrónico aquí usando el servicio de correo
-            mailService.SentEmailRespuestaRecibida(nuevaRespuesta);
-            return new ResponseEntity<>("Respuesta enviada con exito", HttpStatus.CREATED);
-        }catch (Exception e) {
-        	return new ResponseEntity<>("Error al crear una res", HttpStatus.CREATED);
+	@PostMapping("/guardar")
+	public ResponseEntity<?> guardarRespuestaDeComentario(
+			@RequestBody RespuestaComentarioRequest respComentarioRequest) {
+		if (respComentarioRequest.getText().length() > 100) {
+			return new ResponseEntity<>("Debes ingresar menos de 100 caracteres", HttpStatus.BAD_REQUEST);
 		}
-       
-    }
+		try {
+			// Crear una nueva respuesta de comentario
+			RespuestaComentario nuevaRespuesta = new RespuestaComentario(respComentarioRequest.getText(),
+					respComentarioRequest.getUser(), respComentarioRequest.getComentario());
+			// Guardar la nueva respuesta en la base de datos
+			respuestaComentarioRepository.save(nuevaRespuesta);
+			// Puedes enviar un correo electrónico aquí usando el servicio de correo
+			mailService.SentEmailRespuestaRecibida(nuevaRespuesta);
+			return new ResponseEntity<>("Respuesta enviada con exito", HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>("Error al crear una res", HttpStatus.CREATED);
+		}
+
+	}
+
+	@GetMapping("/user/{id}")
+	public Iterable<RespuestaComentario> obtenerRespuestaComentariosPorUserId(@PathVariable(value = "id") Long id) {
+		return respuestaComentarioRepository.findByUserId(id);
+	}
+
+	@GetMapping("/publicacion/{id}")
+	public Iterable<RespuestaComentario> obtenerRespuestaComentariosPorPublicacion(
+			@PathVariable(value = "id") Long id) {
+		return respuestaComentarioRepository.findByPublicacionId(id);
+	}
+
 }
